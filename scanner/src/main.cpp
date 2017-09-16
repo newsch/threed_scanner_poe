d88db19c7fb27962a6674478dd013112afcc038b
Adam Novotny <adam.novotny@postrock.wall>
2017-09-16 17:34:43 -0400

    Added y_servo for 2d scanning

--- scanner/src/main.cpp
+++ scanner/src/main.cpp
@@ -5,28 +5,100 @@
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
+#include "Servo.h"
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+// Variables to control state
+#define STOPPED 0
+#define RUNNING 1
+#define STOP 2
+#define START 3
+
+int cur_state = STOP;
+
+const int x_servo_pin = 9;
+const int y_servo_pin = 10;
+const int ir_sensor_pin = A0;
+
+int servo_pos[2] = { 0, 0 };
+int servo_dir = 1; // 1 or -1 for right or left
+const int servo_min[2] = { 45, 45 };
+const int servo_max[2] = { 135, 135 };
+Servo x_servo;
+Servo y_servo;
+
+long previous_ms = 0;
+long current_ms = 0;
+const int ms_delay = 100;
+
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  x_servo.attach(x_servo_pin);
+  y_servo.attach(y_servo_pin);
+  Serial.begin(9600);
+  Serial.println("Begin");
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+  switch (cur_state) {
+    case STOPPED:
+      if(Serial.read() == 's'){
+        cur_state = START;
+        break;
+      }
+      break;
 
-  // wait for a second
-  delay(1000);
+    case RUNNING:
+      current_ms = millis();
+      if(current_ms > previous_ms + ms_delay){
+        previous_ms = current_ms;
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+        // Read sensor and send data
+        Serial.print(x_servo.read() + "\t");
+        Serial.print(y_servo.read() + "\t");
+        Serial.println(analogRead(ir_sensor_pin));
 
-   // wait for a second
-  delay(1000);
+        // Move servos
+        servo_pos[0] += servo_dir; // Increment x_servo
+        x_servo.write(servo_pos[0]);
+
+        // If x_servo is on bounds increment y_servo
+        if (servo_pos[0] >= servo_max[0] || servo_pos[0] <= servo_min[0]){
+          servo_dir = servo_dir * -1; // Flip direction
+
+          servo_pos[1] +=1;
+          y_servo.write(servo_pos[1]);
+
+          // If y_servo is on bounds, scanning is complete
+          if (servo_pos[1] > servo_max[1]){
+            Serial.println("Complete!");
+            cur_state = STOP;
+            break;
+          }
+        }
+      }
+
+      break;
+
+    case STOP:
+      // Reset the scanner
+      Serial.println("Stopping...");
+      x_servo.write(90);
+      cur_state = STOPPED;
+      Serial.println("Stopped");
+      break;
+
+    case START:
+      // Init scanning
+      servo_pos = servo_min;
+      x_servo.write(servo_pos[0]);
+      y_servo.write(servo_pos[1]);
+      
+      // wait for servos to move to start
+      sleep(500);
+
+      cur_state = RUNNING;
+      break;
+    }
 }
