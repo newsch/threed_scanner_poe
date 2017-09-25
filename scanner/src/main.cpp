ecf637686a76d8dd72f555bc89bedaf3e4d8c5c3
Adam Novotny <adam.novotny@postrock.wall>
2017-09-25 09:10:42 -0400

    Remove Zigzag scanning and clean up code

--- scanner/src/main.cpp
+++ scanner/src/main.cpp
@@ -20,25 +20,52 @@ const int y_servo_pin = 6;
 const int ir_sensor_pin = A0;
 
 int servo_pos[2] = { 0, 0 };
-int servo_dir = 1; // 1 or -1 for right or left
 const int servo_min[2] = { 30, 85 };
-// const int servo_max[2] = { 10, 80 };
 const int servo_max[2] = { 70, 100 };
 Servo x_servo;
 Servo y_servo;
 
 long previous_ms = 0;
 long current_ms = 0;
-const int ms_delay = 100;
+int cur_ms_delay = 0;
+const int x_ms_delay = 100;
+const int y_ms_delay = 400;
 
-int i = 0;
+// int i = 0;
 
+void sendReadout()
+{
+  Serial.print(x_servo.read());
+  Serial.print(" ");
+  Serial.print(y_servo.read());
+  Serial.print(" ");
+  Serial.println(analogRead(ir_sensor_pin));
+  // Serial.println(i);
+  // i++;
+}
+
+void sendConfig()
+{
+  // Send x min, y min, x max, y max
+  Serial.print(servo_min[0]);
+  Serial.print(" ");
+  Serial.print(servo_min[1]);
+  Serial.print(" ");
+  Serial.print(servo_max[0]);
+  Serial.print(" ");
+  Serial.print(servo_max[1]);
+  Serial.println();
+}
+
+void purgeReceiveBuffer()
+{
+  while(Serial.available()){
+    Serial.read();
+  }
+}
 
 void setup()
 {
-  x_servo.attach(x_servo_pin);
-  y_servo.attach(y_servo_pin);
-
   Serial.begin(100000);
   Serial.println("Begin");
 }
@@ -55,29 +82,29 @@ void loop()
 
     case RUNNING:
       current_ms = millis();
-      if(current_ms > previous_ms + ms_delay){
-        i++;
+      // If time has passed
+      if(current_ms > previous_ms + cur_ms_delay){
         previous_ms = current_ms;
 
-        // Move servos
-        servo_pos[0] += servo_dir; // Increment x_servo
-        x_servo.write(servo_pos[0]);
-
         // Read sensor and send data
-        Serial.print(x_servo.read());
-        Serial.print(" ");
-        Serial.print(y_servo.read());
-        Serial.print(" ");
-        Serial.println(analogRead(ir_sensor_pin));
+        sendReadout();
 
-        // If x_servo is on bounds increment y_servo
-        if (servo_pos[0] >= servo_max[0] || servo_pos[0] <= servo_min[0]){
-          servo_dir = servo_dir * -1; // Flip direction
+        // Increment x servo
+        servo_pos[0] += 1;
+        x_servo.write(servo_pos[0]);
+        cur_ms_delay = x_ms_delay;
 
-          servo_pos[1] +=1;
+        // If x_servo is on bounds (This row has completed)
+        if (servo_pos[0] > servo_max[0]){
+          // Reset x servo and increment y servo
+          servo_pos[0] = servo_min[0];
+          servo_pos[1] += 1;
+
+          x_servo.write(servo_pos[0]);
           y_servo.write(servo_pos[1]);
 
-
+          // Set next delay to longer for x_servo to have time to reset to home
+          cur_ms_delay = y_ms_delay;
 
           // If y_servo is on bounds, scanning is complete
           if (servo_pos[1] > servo_max[1]){
@@ -85,17 +112,6 @@ void loop()
             cur_state = STOP;
             break;
           }
-          i++;
-
-          delay(500);
-
-          // Read sensor and send data
-          Serial.print(x_servo.read());
-          Serial.print(" ");
-          Serial.print(y_servo.read());
-          Serial.print(" ");
-          // Serial.println(i);
-          Serial.println(analogRead(ir_sensor_pin));
         }
       }
 
@@ -106,31 +122,34 @@ void loop()
       Serial.println("Stopping...");
       x_servo.write(90);
       y_servo.write(90);
+
+      x_servo.detach();
+      y_servo.detach();
+
       cur_state = STOPPED;
       Serial.println("*** Stopped ***");
+
+      purgeReceiveBuffer();
       break;
 
     case START:
       Serial.println("*** Starting ***");
-      Serial.print(servo_min[0]);
-      Serial.print(" ");
-      Serial.print(servo_min[1]);
-      Serial.print(" ");
-      Serial.print(servo_max[0]);
-      Serial.print(" ");
-      Serial.print(servo_max[1]);
-      Serial.println();
+
+      x_servo.attach(x_servo_pin);
+      y_servo.attach(y_servo_pin);
+
+      sendConfig();
 
       // Init scanning
+      // Set servo_pos to servo_min
       memcpy(servo_pos, servo_min, sizeof servo_min);
-      servo_dir = 1;
-      i = 0;
 
+      // Move servos to starting position
       x_servo.write(servo_pos[0]);
       y_servo.write(servo_pos[1]);
 
-      // wait for servos to move to start
-      delay(500);
+      // set delay before first readout
+      cur_ms_delay = 500;
 
       cur_state = RUNNING;
       break;
