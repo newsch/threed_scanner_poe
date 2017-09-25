48db92e45769e3e4382cfdb532b3cb0871ae90c2
Adam Novotny <adam.novotny@postrock.wall>
2017-09-25 19:33:59 -0400

    Better calibration analysis

--- scanner/analyze.py
+++ scanner/analyze.py
@@ -21,11 +21,8 @@ elif len(sys.argv) == 3:
     calibration_data = np.genfromtxt(sys.argv[1], dtype=np.int32, delimiter=",", skip_header=1)
     scan_data = np.genfromtxt(sys.argv[2], dtype=np.int32, delimiter=",", skip_header=1)
 
-    processed_data = scan_data - calibration_data
-    low_value_indices = processed_data < 0
-    processed_data[low_value_indices] = 0
+    low_value_indices = scan_data - calibration_data < 20
+    scan_data[low_value_indices] = 0
 
-    print(processed_data)
 
     plotArray(scan_data)
-    plotArray(processed_data)
